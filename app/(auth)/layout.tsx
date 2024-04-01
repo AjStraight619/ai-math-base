b1b357d54233e7ba71c857f042d431ef616dd0a8
Alex Straight <alex@Alexs-Laptop.local>
2024-04-01 00:24:22 -0700

    adjusted padding in AuthLayout to look better on small screens.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex items-center justify-center p-24">
+    <div className="min-h-screen flex items-center justify-center md:p-24 p-2">
       {children}
     </div>
   );
