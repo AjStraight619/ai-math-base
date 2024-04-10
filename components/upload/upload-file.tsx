fdb39901782b0f6e586c19e300cd087b5564ca0d
Alex Straight <alex@Alexs-Laptop.local>
2024-04-09 23:37:47 -0700

    removed test-2 page

--- components/upload/upload-file.tsx
+++ components/upload/upload-file.tsx
@@ -1,16 +1,16 @@
-import { PaperclipIcon } from "lucide-react";
-import React, { useRef } from "react";
+import { PaperclipIcon } from 'lucide-react'
+import React, { useRef } from 'react'
 
-type UploadFileProps = {};
+type UploadFileProps = {}
 
 const UploadFile = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null)
   return (
     <>
       <input hidden ref={inputRef} />
       <PaperclipIcon onClick={() => inputRef.current?.click()} />
     </>
-  );
-};
+  )
+}
 
-export default UploadFile;
+export default UploadFile
